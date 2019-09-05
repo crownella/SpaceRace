97f1e0dd50f8e657516e0b6ea0f5b1d8711d2755
Kate <kah703@nyu.edu>
2019-09-05 17:12:25 -0400

    done with clone

--- Assets/BullletController.cs
+++ Assets/BullletController.cs
@@ -1,21 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BullletController : MonoBehaviour
 {
     public Vector2 velocity;
+    public int direction;
+    public float warpPos;
+
+    private Vector2 pos;
+    
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (direction < 0)
+        {
+            warpPos = -warpPos;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(velocity * Time.deltaTime);
+        pos = gameObject.transform.position;
+        transform.Translate(velocity * Time.deltaTime * direction);
+        if (Math.Abs(pos.x - warpPos) < .05) 
+        {
+            pos.x = -warpPos;
+            transform.position = pos;
+        }
     }
 }
